d431c107d0f50be7583037452867ac1903e57b0e
sss-0916 <sss-0916@qq.com>
2018-11-05 11:47:30 +0800

    代码提交

---
+++ lexicographical_compare的用法.cpp
@@ -0,0 +1,43 @@
+#include "iostream"
+#include "algorithm"
+#include "list"
+#include "vector"
+using namespace std;
+
+void print(int& ele)
+{
+	cout << ele << " ";
+}
+
+int main()
+{
+	list<int> l1;
+	vector<int> l2;
+	l1.push_back(1);
+	l1.push_back(2);
+	l1.push_back(3);
+	l1.push_back(4);
+	l1.push_back(5);
+	l2.push_back(1);
+	l2.push_back(2);
+	l2.push_back(3);
+	l2.push_back(5);
+	l2.push_back(4);
+	cout << "l1: " << endl;
+	for_each(l1.begin(), l1.end(), print);
+	cout << endl;
+	cout << "l2: " << endl;
+	for_each(l2.begin(), l2.end(), print);
+	cout << endl;
+	bool b = lexicographical_compare(l1.begin(), l1.end(), l2.begin(), l2.end());
+	if (b)
+	{
+		cout << "l1 < l2. " << endl;
+	}
+	else
+	{
+		cout << "l1 >= l2. " << endl;
+	}
+	system("pause");
+	return 0;
+}
